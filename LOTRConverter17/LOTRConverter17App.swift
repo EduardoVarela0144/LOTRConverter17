e2ace41d7395a0c455703b53cc441c9a48dca9f0
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2024-03-16 22:04:10 -0600

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Eduardo Varela Hernández on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
