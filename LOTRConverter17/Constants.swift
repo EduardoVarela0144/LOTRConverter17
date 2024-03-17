a64dee26e11e23b8966a844470bfb43d1e60a325
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2024-03-17 15:43:35 -0600

    Lesson 7 - Duplicating Our Exchange Rate View

---
+++ LOTRConverter17/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  LOTRConverter17
+//
+//  Created by Eduardo Varela Hernández on 17/03/24.
+//
+
+import Foundation
---
+++ LOTRConverter17/ExchangeRate.swift
@@ -0,0 +1,8 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter17
+//
+//  Created by Eduardo Varela Hernández on 17/03/24.
+//
+
+import Foundation
