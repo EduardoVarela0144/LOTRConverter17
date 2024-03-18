8f3be3c3cd8bd43fee348ac1efec955baedd2962
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2024-03-18 00:25:08 -0600

    Lesson 9 - Laying Out Our Select Currency Screen

---
+++ LOTRConverter17/CurrencyIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyIcon.swift
+//  LOTRConverter17
+//
+//  Created by Eduardo Varela Hernández on 18/03/24.
+//
+
+import SwiftUI
+
+struct CurrencyIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyIcon()
+}
---
+++ LOTRConverter17/SelectCurrency.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter17
+//
+//  Created by Eduardo Varela Hernández on 17/03/24.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
